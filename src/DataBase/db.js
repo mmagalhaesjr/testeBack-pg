b7468553151e34f28c8ff0a38e243f9c3b94bb7f
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-08-23 10:23:56 -0300

    pronto para o render

--- src/DataBase/db.js
+++ src/DataBase/db.js
@@ -4,22 +4,22 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL,
-  ...(process.env.NODE_ENV === "prod" && {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }),
-};
-const db = new Pool(configDatabase);
+//const configDatabase = {
+//  connectionString: process.env.DATABASE_URL,
+//  ...(process.env.NODE_ENV === "prod" && {
+//    ssl: {
+//     rejectUnauthorized: false,
+//    },
+//  }),
+//};
+// const db = new Pool(configDatabase);
 
 
 
-// const db = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl:true
-// })
+const db = new Pool({
+connectionString: process.env.DATABASE_URL,
+ssl:true
+})
 
 
 
