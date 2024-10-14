81a3dddd4b454a4b3c524485e670c806bc9663ed
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-10-14 10:58:20 -0300

    up

--- src/DataBase/db.js
+++ src/DataBase/db.js
@@ -1,29 +1,52 @@
+// para usar em localhost----------------
+
 import pg from 'pg';
 import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pg;
 
-// const configDatabase = {
-//  connectionString: process.env.DATABASE_URL,
-//  ...(process.env.NODE_ENV === "prod" && {
-//    ssl: {
-//     rejectUnauthorized: false,
-//    },
-//  }),
-// };
-// const db = new Pool(configDatabase);
-
-
-
-const db = new Pool({
-connectionString: process.env.DATABASE_URL,
-ssl:true
-})
-
+const configDatabase = {
+ connectionString: process.env.DATABASE_URL,
+ ...(process.env.NODE_ENV === "prod" && {
+   ssl: {
+    rejectUnauthorized: false,
+   },
+ }),
+};
+const db = new Pool(configDatabase);
 
 
 export default db;
+
+
+// para usar no render-----------------------
+
+// import pg from 'pg';
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const { Pool } = pg;
+
+
+// const db = new Pool({
+// connectionString: process.env.DATABASE_URL,
+// ssl:true
+// })
+
+// export default db;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,52 +1,52 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import autenticarRotas from "./Routes/AutenticarRotas.js";
-// import tarefasRotas from "./Routes/TarefasRotas.js";
-
-
-
-// dotenv.config()
-
-
-// const server = express().use(cors());
-// server.use(express.json())
-
-
-// server.use(autenticarRotas)
-// server.use(tarefasRotas)
-
-
-// const porta = process.env.PORTA || 5000;
-
-// server.listen(porta, () => {
-//     console.log(`*** Servidor rodando na porta ${porta} ***`);
-// });
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import autenticarRotas from "./Routes/AutenticarRotas.js";
 import tarefasRotas from "./Routes/TarefasRotas.js";
 
-dotenv.config();
-
-const server = express();
 
 
-const corsOptions = {
-    origin: 'https://testefront-theta.vercel.app',
-    optionsSuccessStatus: 200 
-};
+dotenv.config()
 
-server.use(cors(corsOptions));
-server.use(express.json());
 
-server.use(autenticarRotas);
-server.use(tarefasRotas);
+const server = express().use(cors());
+server.use(express.json())
+
+
+server.use(autenticarRotas)
+server.use(tarefasRotas)
+
 
 const porta = process.env.PORTA || 5000;
 
 server.listen(porta, () => {
     console.log(`*** Servidor rodando na porta ${porta} ***`);
 });
+
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import autenticarRotas from "./Routes/AutenticarRotas.js";
+// import tarefasRotas from "./Routes/TarefasRotas.js";
+
+// dotenv.config();
+
+// const server = express();
+
+
+// const corsOptions = {
+//     origin: 'https://testefront-theta.vercel.app',
+//     optionsSuccessStatus: 200 
+// };
+
+// server.use(cors(corsOptions));
+// server.use(express.json());
+
+// server.use(autenticarRotas);
+// server.use(tarefasRotas);
+
+// const porta = process.env.PORTA || 5000;
+
+// server.listen(porta, () => {
+//     console.log(`*** Servidor rodando na porta ${porta} ***`);
+// });
